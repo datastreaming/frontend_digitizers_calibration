da3b5f35403419a0d85e258b0e409d40976003af
ebner <simon.ebner@psi.ch>
2017-10-19 17:37:07 +0200

    calculation.py

--- calculation.py
+++ calculation.py
@@ -9,13 +9,13 @@ calibration_data = vcal_class('comb007-5120.vcal')
 
 
 # trigger_cell between 0 and 1023
-trigger_cell = 0
+# trigger_cell = 0
 # channel = 15
 channel = 15
 
 with source(channels=['SARFE10-PBPG050:HAMP-014-x-h1-DATA', 'SARFE10-PBPG050:HAMP-014-x-h1-BG-DATA', 'SARFE10-PBPG050:HAMP-014-x-h1-DRS_TC']) as stream:
     while True:
-        # start = time.time()
+
         message = stream.receive()
         data = message.data.data['SARFE10-PBPG050:HAMP-014-x-h1-DATA'].value
         background = message.data.data['SARFE10-PBPG050:HAMP-014-x-h1-BG-DATA'].value
@@ -33,7 +33,4 @@ with source(channels=['SARFE10-PBPG050:HAMP-014-x-h1-DATA', 'SARFE10-PBPG050:HAM
 
         data = data.sum()
 
-        # elapsed = time.time() - start
-
-        # print(elapsed)
         print(pulse_id, data)
