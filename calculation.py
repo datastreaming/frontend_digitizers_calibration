df3f0a901af117f9df326225776d8d228946f876
ebner <simon.ebner@psi.ch>
2017-10-19 12:10:22 +0200

    first version

--- calculation.py
+++ calculation.py
@@ -1,6 +1,26 @@
 
 from bsread import source
-with source(channels=['YOUR_CHANNEL', 'YOUR_SECOND_CHANNEL']) as stream:
+
+
+from drs_vcal_tcal import tcal_class, vcal_class
+calibration_data = vcal_class('comb007-5120.vcal')
+
+
+# trigger_cell between 0 and 1023
+trigger_cell = 0
+# channel = 15
+channel = 15
+
+with source(channels=['SARFE10-PBPG050:HAMP-014-x-h1-DATA', 'SARFE10-PBPG050:HAMP-014-x-h1-BG-DATA']) as stream:
     while True:
         message = stream.receive()
-        print(message.data.data['YOUR_CHANNEL'].value)
+        data = message.data.data['SARFE10-PBPG050:HAMP-014-x-h1-DATA'].value
+        background = message.data.data['SARFE10-PBPG050:HAMP-014-x-h1-BG-DATA'].value
+        pulse_id = message.data.pulse_id
+
+        data = calibration_data.calibrate(data, trigger_cell, channel)
+
+        print(data)
+
+
+
