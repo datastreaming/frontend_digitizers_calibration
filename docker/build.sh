5d07cc5211786ef810d1d5988fee5299c2a9384c
Andrej Babic <andrej.babic@psi.ch>
2017-11-13 11:54:49 +0100

    Move config and add config to docker

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 VERSION=0.0.2
-docker build --no-cache=true -t docker.psi.ch:5000/cam_server .
+docker build --no-cache=true -t docker.psi.ch:5000/frontend_digitizers_calibration .
 docker tag docker.psi.ch:5000/frontend_digitizers_calibration docker.psi.ch:5000/frontend_digitizers_calibration:$VERSION
 docker push docker.psi.ch:5000/frontend_digitizers_calibration:$VERSION
 docker push docker.psi.ch:5000/frontend_digitizers_calibration
