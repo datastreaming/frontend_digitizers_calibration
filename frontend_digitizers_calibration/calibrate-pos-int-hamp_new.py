1047952f73142e45e9ef9a06b2004ceb8bc66d79
ebner <simon.ebner@psi.ch>
2017-11-13 15:08:38 +0100

    new version pbpg

---
+++ frontend_digitizers_calibration/calibrate-pos-int-hamp_new.py
@@ -0,0 +1,199 @@
+from bsread import source
+from collections import deque
+import numpy
+import time
+import epics
+
+from epics import caget, caput
+from frontend_digitizers_calibration.drs_vcal_tcal import tcal_class, vcal_class
+calibration_data = vcal_class('docker/cfg/comb006-2498.vcal')
+
+
+channel_numbers = [15, 14, 13, 12]
+required_channels = []
+for n in channel_numbers:
+    required_channels.append("SARFE10-CVME-PHO6211:Lnk9Ch%d-DATA" % n)
+    required_channels.append("SARFE10-CVME-PHO6211:Lnk9Ch%d-BG-DATA" % n)
+    required_channels.append("SARFE10-CVME-PHO6211:Lnk9Ch%d-DRS_TC" % n)
+    required_channels.append("SARFE10-CVME-PHO6211:Lnk9Ch%d-BG-DRS_TC" % n)
+
+
+# # trigger_cell between 0 and 1023
+# # trigger_cell = 0
+# # channel = 15
+# channel1 = 15
+# channel2 = 14
+# channel3 = 13
+# channel4 = 12
+
+
+queue = deque(maxlen=240)
+
+#
+# required_channels = ['SARFE10-CVME-PHO6211:Lnk9Ch15-DATA', 'SARFE10-CVME-PHO6211:Lnk9Ch15-BG-DATA', 'SARFE10-CVME-PHO6211:Lnk9Ch15-DRS_TC', 'SARFE10-CVME-PHO6211:Lnk9Ch15-BG-DRS_TC',
+#                      'SARFE10-CVME-PHO6211:Lnk9Ch14-DATA', 'SARFE10-CVME-PHO6211:Lnk9Ch14-BG-DATA', 'SARFE10-CVME-PHO6211:Lnk9Ch14-DRS_TC', 'SARFE10-CVME-PHO6211:Lnk9Ch14-BG-DRS_TC',
+#                      'SARFE10-CVME-PHO6211:Lnk9Ch13-DATA', 'SARFE10-CVME-PHO6211:Lnk9Ch13-BG-DATA', 'SARFE10-CVME-PHO6211:Lnk9Ch13-DRS_TC', 'SARFE10-CVME-PHO6211:Lnk9Ch13-BG-DRS_TC',
+#                      'SARFE10-CVME-PHO6211:Lnk9Ch12-DATA', 'SARFE10-CVME-PHO6211:Lnk9Ch12-BG-DATA', 'SARFE10-CVME-PHO6211:Lnk9Ch12-DRS_TC', 'SARFE10-CVME-PHO6211:Lnk9Ch12-BG-DRS_TC'
+#                      ]
+
+with source(channels=required_channels) as stream:
+    while True:
+
+        message = stream.receive()
+
+        pulse_id = message.data.pulse_id
+
+        data = []
+        background = []
+
+        for index, channel_number in enumerate(channel_numbers):
+            channel_data = message.data.data['SARFE10-CVME-PHO6211:Lnk9Ch%d-DATA' % channel_number].value
+            channel_background = message.data.data['SARFE10-CVME-PHO6211:Lnk9Ch%d-BG-DATA' % channel_number].value
+            channel_data_trigger_cell = message.data.data['SARFE10-CVME-PHO6211:Lnk9Ch%d-DRS_TC' % channel_number].value
+            channel_background_trigger_cell = message.data.data['SARFE10-CVME-PHO6211:Lnk9Ch%d-BG-DRS_TC' % channel_number].value
+
+            # offset and scaling
+            channel_data = (channel_data.astype(numpy.float32) - 0x800) / 4096
+            channel_background = (channel_background.astype(numpy.float32) - 0x800) / 4096
+
+            # calibration
+            channel_data = calibration_data.calibrate(channel_data, channel_data_trigger_cell, channel_number)
+            channel_background = calibration_data.calibrate(channel_background, channel_background_trigger_cell, channel_number)
+
+            caput('SARFE10-CVME-PHO6211:Lnk9Ch%d-DATA-CALIBRATED' % channel_number, channel_data)
+            caput('SARFE10-CVME-PHO6211:Lnk9Ch%d-BG-DATA-CALIBRATED' % channel_number, channel_background)
+
+            # background susbstracion
+            channel_data -= channel_background
+
+            # integration
+            channel_data = channel_data.sum()
+
+            caput('SARFE10-CVME-PHO6211:Lnk9Ch%d-DATA-SUM' % channel_number, channel_data)
+
+            data.append(channel_data)
+            background.append(channel_background)
+
+        # intensity and position calculations
+        intensity = (data[0] + data[1] + data[2] + data[3]) / (-2)
+        position1 = ((((data[0] - data[1]) / (data[0] + data[1])) - (-0.2115)) / -0.0291) - 0.4
+        position2 = ((((data[2] - data[3]) / (data[2] + data[3])) - (-0.1632)) / 0.0161) + 0.2
+
+        caput('SARFE10-PBPG050:HAMP-XPOS', position1)
+        caput('SARFE10-PBPG050:HAMP-YPOS', position2)
+        caput('SARFE10-PBPG050:HAMP-INTENSITY', intensity)
+
+        queue.append(intensity)
+        # average last 240 intensities
+        intensity_average = sum(queue) / len(queue)
+
+        caput('SARFE10-PBPG050:HAMP-INTENSITY-AVG', intensity_average)
+
+        intensity_ds = caget('SARFE10-PBPG050:PHOTON-ENERGY-PER-PULSE-DS')
+        intensity_us = caget('SARFE10-PBPG050:PHOTON-ENERGY-PER-PULSE-US')
+        intensity_avg_keithley = (intensity_ds + intensity_us) / 2
+        intensity_cal = intensity * (intensity_avg_keithley / intensity_average)
+        caput('SARFE10-PBPG050:HAMP-INTENSITY-CAL', intensity_cal)
+
+        print(pulse_id)
+
+        # # get data from stream
+        # data1 = message.data.data['SARFE10-CVME-PHO6211:Lnk9Ch15-DATA'].value
+        # background1 = message.data.data['SARFE10-CVME-PHO6211:Lnk9Ch15-BG-DATA'].value
+        # data1_trigger_cell = message.data.data['SARFE10-CVME-PHO6211:Lnk9Ch15-DRS_TC'].value
+        # background1_trigger_cell = message.data.data['SARFE10-CVME-PHO6211:Lnk9Ch15-BG-DRS_TC'].value
+        #
+        # data2 = message.data.data['SARFE10-CVME-PHO6211:Lnk9Ch14-DATA'].value
+        # background2 = message.data.data['SARFE10-CVME-PHO6211:Lnk9Ch14-BG-DATA'].value
+        # data2_trigger_cell = message.data.data['SARFE10-CVME-PHO6211:Lnk9Ch14-DRS_TC'].value
+        # background2_trigger_cell = message.data.data['SARFE10-CVME-PHO6211:Lnk9Ch14-BG-DRS_TC'].value
+        #
+        # data3 = message.data.data['SARFE10-CVME-PHO6211:Lnk9Ch13-DATA'].value
+        # background3 = message.data.data['SARFE10-CVME-PHO6211:Lnk9Ch13-BG-DATA'].value
+        # data3_trigger_cell = message.data.data['SARFE10-CVME-PHO6211:Lnk9Ch13-DRS_TC'].value
+        # background3_trigger_cell = message.data.data['SARFE10-CVME-PHO6211:Lnk9Ch13-BG-DRS_TC'].value
+        #
+        # data4 = message.data.data['SARFE10-CVME-PHO6211:Lnk9Ch12-DATA'].value
+        # background4 = message.data.data['SARFE10-CVME-PHO6211:Lnk9Ch12-BG-DATA'].value
+        # data4_trigger_cell = message.data.data['SARFE10-CVME-PHO6211:Lnk9Ch12-DRS_TC'].value
+        # background4_trigger_cell = message.data.data['SARFE10-CVME-PHO6211:Lnk9Ch12-BG-DRS_TC'].value
+
+
+
+        # # offset and scaling
+        # background1 = (background1.astype(numpy.float32)-0x800)/4096
+        # data1 = (data1.astype(numpy.float32)-0x800)/4096
+        #
+        # background2 = (background2.astype(numpy.float32)-0x800)/4096
+        # data2 = (data2.astype(numpy.float32)-0x800)/4096
+        #
+        # background3 = (background3.astype(numpy.float32)-0x800)/4096
+        # data3 = (data3.astype(numpy.float32)-0x800)/4096
+        #
+        # background4 = (background4.astype(numpy.float32)-0x800)/4096
+        # data4 = (data4.astype(numpy.float32)-0x800)/4096
+
+        # # calibration
+        # background1 = calibration_data.calibrate(background1, background1_trigger_cell, channel1)
+        # data1 = calibration_data.calibrate(data1, data1_trigger_cell, channel1)
+        # caput('SARFE10-CVME-PHO6211:Lnk9Ch15-DATA-CALIBRATED', data1)
+        # caput('SARFE10-CVME-PHO6211:Lnk9Ch15-BG-DATA-CALIBRATED', background1)
+        #
+        # background2 = calibration_data.calibrate(background2, background2_trigger_cell, channel2)
+        # data2 = calibration_data.calibrate(data2, data2_trigger_cell, channel2)
+        # caput('SARFE10-CVME-PHO6211:Lnk9Ch14-DATA-CALIBRATED', data2)
+        # caput('SARFE10-CVME-PHO6211:Lnk9Ch14-BG-DATA-CALIBRATED', background2)
+        #
+        # background3 = calibration_data.calibrate(background3, background3_trigger_cell, channel3)
+        # data3 = calibration_data.calibrate(data3, data3_trigger_cell, channel3)
+        # caput('SARFE10-CVME-PHO6211:Lnk9Ch13-DATA-CALIBRATED', data3)
+        # caput('SARFE10-CVME-PHO6211:Lnk9Ch13-BG-DATA-CALIBRATED', background3)
+        #
+        # background4 = calibration_data.calibrate(background4, background4_trigger_cell, channel4)
+        # data4 = calibration_data.calibrate(data4, data4_trigger_cell, channel4)
+        # caput('SARFE10-CVME-PHO6211:Lnk9Ch12-DATA-CALIBRATED', data4)
+        # caput('SARFE10-CVME-PHO6211:Lnk9Ch12-BG-DATA-CALIBRATED', background4)
+
+        # # background susbstracion
+        # data1 -= background1
+        # data2 -= background2
+        # data3 -= background3
+        # data4 -= background4
+
+        # # integration
+        # data1 = data1.sum()
+        # caput('SARFE10-CVME-PHO6211:Lnk9Ch15-DATA-SUM', data1)
+        # data2 = data2.sum()
+        # caput('SARFE10-CVME-PHO6211:Lnk9Ch14-DATA-SUM', data2)
+        # data3 = data3.sum()
+        # caput('SARFE10-CVME-PHO6211:Lnk9Ch13-DATA-SUM', data3)
+        # data4 = data4.sum()
+        # caput('SARFE10-CVME-PHO6211:Lnk9Ch12-DATA-SUM', data4)
+
+
+
+#         # intensity and position calculations
+#         intensity = (data1 + data2 + data3 + data4)/(-2)
+# #        position1 = ((data1 - data2)/(data1 + data2))
+#         position1 = ((((data1 - data2)/(data1 + data2))-(-0.2115))/-0.0291)-0.4
+# #        position2 = ((data3 - data4)/(data3 + data4))
+#         position2 = ((((data3 - data4)/(data3 + data4))-(-0.1632))/0.0161)+0.2
+#
+#         caput('SARFE10-PBPG050:HAMP-XPOS', position1)
+#         caput('SARFE10-PBPG050:HAMP-YPOS', position2)
+#
+#         caput('SARFE10-PBPG050:HAMP-INTENSITY', intensity)
+#
+#         queue.append(intensity)
+# #        print(queue)
+#         intensity_average = sum(queue)/len(queue)
+#         caput('SARFE10-PBPG050:HAMP-INTENSITY-AVG', intensity_average)
+#         intensity_ds = caget('SARFE10-PBPG050:PHOTON-ENERGY-PER-PULSE-DS')
+#         intensity_us = caget('SARFE10-PBPG050:PHOTON-ENERGY-PER-PULSE-US')
+#         intensity_avg_keithley = (intensity_ds+intensity_us)/2
+#         intensity_cal = intensity*( intensity_avg_keithley/intensity_average)
+#         caput('SARFE10-PBPG050:HAMP-INTENSITY-CAL', intensity_cal)
+#
+# #        print(intensity_sum)
+# #        print(intensity_sum)
+#         print(pulse_id)
