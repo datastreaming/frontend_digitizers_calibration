859e7b1a899e53a3d1f9495dae99baf4fe0f80e3
Andrej Babic <andrej.babic@psi.ch>
2017-11-13 14:31:27 +0100

    Refactor calibrate pbps

---
+++ tests/test_calibrate_pbps.py
@@ -0,0 +1,8 @@
+import unittest
+
+from frontend_digitizers_calibration.calibrate_pbps import start_stream
+
+
+class TestCalibratePbps(unittest.TestCase):
+    def test_simple_transmission(self):
+        pass
