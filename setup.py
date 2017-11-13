1a35d699dec6050029e9ea7a5027cb9af4934579
Andrej Babic <andrej.babic@psi.ch>
2017-11-13 17:45:18 +0100

    Increase library version
    - First production version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="frontend_digitizers_calibration",
-      version="0.0.4",
+      version="0.0.5",
       author="Paul Scherrer Institute",
       description="Arturo will fill this out.",
       license="GPL3",
