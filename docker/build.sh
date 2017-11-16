1f4dffcfb3505a11ab7f03726dd3c92190aa1138
Andrej Babic <andrej.babic@psi.ch>
2017-11-16 13:44:48 +0100

    New library version
    - Fixed small bugs
    - Logging improvement

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=1.1.0
+VERSION=1.1.1
 docker build --no-cache=true -t docker.psi.ch:5000/frontend_digitizers_calibration .
 docker tag docker.psi.ch:5000/frontend_digitizers_calibration docker.psi.ch:5000/frontend_digitizers_calibration:$VERSION
 docker push docker.psi.ch:5000/frontend_digitizers_calibration:$VERSION
