0b6da12f3aa58262b2e454a8b9a5703a31e7e751
Andrej Babic <andrej.babic@psi.ch>
2017-11-17 16:15:14 +0100

    New library version
    - Intensity field in PBPG

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=1.1.4
+VERSION=1.1.5
 docker build --no-cache=true -t docker.psi.ch:5000/frontend_digitizers_calibration .
 docker tag docker.psi.ch:5000/frontend_digitizers_calibration docker.psi.ch:5000/frontend_digitizers_calibration:$VERSION
 docker push docker.psi.ch:5000/frontend_digitizers_calibration:$VERSION
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="frontend_digitizers_calibration",
-      version="1.1.4",
+      version="1.1.5",
       author="Paul Scherrer Institute",
       description="Arturo will fill this out.",
       license="GPL3",
