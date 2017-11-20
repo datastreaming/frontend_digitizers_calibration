d7853e8e3cdfa8f3c3c8f966aa5a52ff66006155
Andrej Babic <andrej.babic@psi.ch>
2017-11-20 15:40:54 +0100

    Add single channel functionality

--- frontend_digitizers_calibration/devices/mapping.py
+++ frontend_digitizers_calibration/devices/mapping.py
@@ -1,7 +1,9 @@
 from frontend_digitizers_calibration.devices.pbpg import process_pbpg
 from frontend_digitizers_calibration.devices.pbps import process_pbps
+from frontend_digitizers_calibration.devices.single_channel import process_single_channel
 
 device_type_processing_function_mapping = {
     "pbps": process_pbps,
-    "pbpg": process_pbpg
+    "pbpg": process_pbpg,
+    "single_channel": process_single_channel
 }
---
+++ frontend_digitizers_calibration/devices/single_channel.py
@@ -0,0 +1,38 @@
+from frontend_digitizers_calibration import config
+from frontend_digitizers_calibration.devices.utils import calibrate_channel, SUFFIX_CHANNEL_DATA_SUM
+from frontend_digitizers_calibration.utils import notify_epics
+
+
+SUFFIX_DEVICE_SCALED_DATA_SUM = "SCALED-DATA-SUM"
+
+
+def process_single_channel(message, device_name, device_definition, channels_definition, calibration_data):
+
+    scaling_offset = device_definition["scaling_offset"]
+    scaling_factor = device_definition["scaling_factor"]
+    pv_prefix = None
+
+    data_to_send = {}
+
+    for channel in channels_definition:
+        nonlocal pv_prefix
+        pv_prefix = channel[config.CONFIG_CHANNEL_PV_PREFIX]
+        channel_number = channel[config.CONFIG_CHANNEL_NUMBER]
+        pv_names = channel[config.CONFIG_CHANNEL_PVS]
+
+        calibrate_channel(message=message,
+                          data_to_send=data_to_send,
+                          pv_prefix=pv_prefix,
+                          channel_number=channel_number,
+                          pv_names=pv_names,
+                          calibration_data=calibration_data)
+
+    # The data sum for the single channel should be scaled.
+    data_sum = data_to_send[pv_prefix + SUFFIX_CHANNEL_DATA_SUM]
+    scaled_data_sum = (data_sum * scaling_factor) + scaling_offset
+    data_to_send[device_name + SUFFIX_DEVICE_SCALED_DATA_SUM] = scaled_data_sum
+
+    # Notify EPICS channels with the new calculated data.
+    notify_epics(data_to_send)
+
+    return data_to_send
