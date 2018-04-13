7a05c3e10bf1e3f0aeb0365670e923cd9fd9ff95
Andrej Babic <andrej.babic@psi.ch>
2018-04-13 10:31:00 +0200

    Update docker build

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=2.0.0
+VERSION=2.0.2
 docker build --no-cache=true -t docker.psi.ch:5000/frontend_digitizers_calibration .
 docker tag docker.psi.ch:5000/frontend_digitizers_calibration docker.psi.ch:5000/frontend_digitizers_calibration:$VERSION
 docker push docker.psi.ch:5000/frontend_digitizers_calibration:$VERSION
